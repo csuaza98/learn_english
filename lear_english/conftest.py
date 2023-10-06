1a4fc394dd53bebd18a805ac9dd30886f6f84d1f
Carlos Suaza <fruco1234@gmail.com>
2023-10-06 17:38:18 -0500

    First awesome commit

---
+++ lear_english/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from lear_english.users.models import User
+from lear_english.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
